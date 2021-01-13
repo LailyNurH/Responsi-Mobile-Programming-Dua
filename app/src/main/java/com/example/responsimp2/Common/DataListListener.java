506a2213e03bbcbcda7963861916d254263d9bbc
LailyNurH <lailyhamidah3005@gmail.com>
2021-01-13 15:04:06 +0700

    ResponsiMP

--- app/src/main/java/com/example/responsimp2/Common/DataListListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.responsimp2.Common;
-
-
-import com.example.responsimp2.MatKulDao;
-import com.example.responsimp2.model.MataKuliah;
-
-public interface DataListListener {
-    void onRemoveClick(MataKuliah mataKuliah);
-}
